d3b9ecebdcc3de98ebb213d1221a389fb959eec4
Thamilton8383 <thamilton8383@gmail.com>
2025-06-22 09:33:46 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>ATS Dashboard</h1>
+      <p style={{ marginTop: '1rem' }}>Welcome to your live College Football ATS prediction dashboard!</p>
+    </main>
+  );
+}
